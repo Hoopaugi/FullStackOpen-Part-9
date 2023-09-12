25311cf459ec40e745be219f82a0c79465898b8c
Hoopaugi <hoopaugi@gmail.com>
2023-09-12 07:44:52 +0300

    Diary Refactoring

--- diary/backend/src/app.ts
+++ diary/backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import pingRouter from './ping';
 import apiRouter from './api';
@@ -6,6 +7,7 @@ import apiRouter from './api';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(pingRouter);
 app.use('/api', apiRouter);
