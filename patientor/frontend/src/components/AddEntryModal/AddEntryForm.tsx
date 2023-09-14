858d43486f66505aaddddf046c56d5544a16dbf3
Hoopaugi <hoopaugi@gmail.com>
2023-09-14 14:01:08 +0300

    Patientor progress

--- patientor/frontend/src/components/AddEntryModal/AddEntryForm.tsx
+++ patientor/frontend/src/components/AddEntryModal/AddEntryForm.tsx
@@ -45,6 +45,9 @@ const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
 
   const [healthCheckRating, setHealtchCheckRating] = useState(HealthCheckRating.Healthy);
 
+  const [dischargeDate, setDischargeDate] = useState('');
+  const [criteria, setCriteria] = useState('');
+
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
 
@@ -102,6 +105,22 @@ const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
       diagnosisCodes,
       healthCheckRating,
       type: 'HealthCheck'
+    });
+  };
+
+  const addHospitalEntry = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    onSubmit({
+      description,
+      date,
+      specialist,
+      diagnosisCodes,
+      discharge: {
+        date: dischargeDate,
+        criteria
+      },
+      type: 'Hospital'
     });
   };
 
@@ -214,7 +233,103 @@ const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
       )
     case 'Hospital':
       return (
-        <></>
+        <div>
+        <form onSubmit={addHospitalEntry}>
+        <Select
+            label="Type"
+            fullWidth
+            value={type}
+            onChange={onTypeChange}
+          >
+          {typeOptions.map(option =>
+            <MenuItem
+              key={option.label}
+              value={option.value}
+            >
+              {option.label
+            }</MenuItem>
+          )}
+          </Select>
+          <TextField
+            label="description"
+            fullWidth 
+            value={description}
+            onChange={({ target }) => setDescription(target.value)}
+          />
+          <TextField
+            label="Date"
+            type="date"
+            fullWidth
+            value={date}
+            onChange={({ target }) => setDate(target.value)}
+          />
+          <TextField
+            label="Specialist"
+            fullWidth
+            value={specialist}
+            onChange={({ target }) => setSpecialist(target.value)}
+          />
+          <InputLabel id="diagnosis-codes-label">Diagnoses</InputLabel>
+          <Select
+            labelId="diagnosis-codes-label"
+            id="diagnosis-codes"
+            label="Diagnoses"
+            multiple
+            fullWidth
+            value={diagnosisCodes}
+            onChange={onDiagnosisCodesChange}
+            input={<OutlinedInput label="Tag" />}
+            renderValue={(selected) => selected.join(', ')}
+          >
+          {diagnoses.map((diagnosis) => (
+            <MenuItem key={diagnosis.code} value={diagnosis.code}>
+              <Checkbox checked={diagnosisCodes.indexOf(diagnosis.code) > -1} />
+              <ListItemText primary={diagnosis.code} />
+            </MenuItem>
+          ))}
+          </Select>
+          <InputLabel>Discharge</InputLabel>
+          <TextField
+            label="Criteria"
+            fullWidth 
+            value={criteria}
+            onChange={({ target }) => setCriteria(target.value)}
+          />
+          <TextField
+            label="Date"
+            type="date"
+            fullWidth
+            value={dischargeDate}
+            onChange={({ target }) => setDischargeDate(target.value)}
+          />
+
+  
+          <Grid>
+            <Grid item>
+              <Button
+                color="secondary"
+                variant="contained"
+                style={{ float: "left" }}
+                type="button"
+                onClick={onCancel}
+              >
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                style={{
+                  float: "right",
+                }}
+                type="submit"
+                variant="contained"
+              >
+                Add
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
       )
     case 'OccupationalHealthcare':
       return (
